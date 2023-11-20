fbaa6b3b7219c6b584bd7fc2ac903b46ccab893b
rafael-isidro <rafa-isidro@hotmail.com>
2023-11-20 10:55:41 -0300

    Exercício 02 - Tipos Utilitários - Finalizado

--- 02/scripts.ts
+++ 02/scripts.ts
@@ -19,3 +19,21 @@ type Carrinho = {
     tipoTransacao: 'credito' | 'Debito',
     cartao: Cartao
 }
+
+type Endereco = {
+    cep: string,
+    rua: string,
+    bairro: string,
+    cidade: string,
+    estado: string
+}
+
+type CarrinhoComEndereco = {
+    item: Item,
+    qtd: number,
+    desconto: number,
+    frete: number,
+    tipoTransacao: Lowercase<'credito' | 'Debito'>,
+    cartao: Cartao,
+    endereco: Endereco
+}
